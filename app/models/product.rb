6ddb290f7a15dfa8f3ff2c618002f0fddf8a64a5
Mischa Schindowski <mischa.schindowski@screenconcept.ch>
2010-07-05 15:05:08 +0200

    added a layout and ported to haml

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,11 @@
 class Product < ActiveRecord::Base
+  default_scope order('title')
+
   validates :title, :description, :image_url, :presence => true
-  validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
+  #validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
+  validates_numericality_of :price, :greater_than_or_equal_to => 0.01, :message => "Should be a number greather or equal a cent, like 12.34"
   validates :title, :uniqueness => true
+  validates :title, :length => {:minimum => 10}
   validates :image_url, :format => {
     :with => %r{\.(gif|jpg|png)$}i,
     :message => 'must be a URL for GIF, JPG or PNG image.'
