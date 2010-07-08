e6028a82f1cc8b46de4e3183c024ea8003526735
Mischa Schindowski <mischa.schindowski@screenconcept.ch>
2010-07-08 15:47:28 +0200

    cart improvements

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -3,6 +3,8 @@ class StoreController < ApplicationController
     @time = Time.now
     @count = increment_count
     @products = Product.all
+    #@lineitemstest = line_items.where(:product_id => product_id).first # felipe fragen!
+    @lineitemstest = LineItem.all
   end
 
   def increment_count
